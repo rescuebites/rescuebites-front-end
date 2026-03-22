21f04a033125d916a3260921b8a4bbd8628b863a
mroja <mrojaslopez03@gmail.com>
2026-03-22 13:44:25 -0300

    feat: add hooks for client profile editing #15 #3

---
+++ src/modules/client/hooks/useUpdateClient.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateClient } from "../api/client.api";
+import { UpdateClientParams } from "../interfaces/requests/updateClient.interface";
+import { useSnackbarStore } from "@/shared/hooks/useSnackbarStore";
+import { useNavigate } from "react-router-dom";
+
+export const useUpdateClient = () => {
+  const queryClient = useQueryClient();
+  const { showMessage } = useSnackbarStore();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: (params: UpdateClientParams) => updateClient(params),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["client", data.clientId] });
+      showMessage("Perfil actualizado exitosamente", "success");
+      navigate("/customer/profile");
+    },
+    onError: (error: any) => {
+      const errorMessage =
+        error?.response?.data?.message || "Error al actualizar el perfil";
+      showMessage(errorMessage, "error");
+    },
+  });
+};
