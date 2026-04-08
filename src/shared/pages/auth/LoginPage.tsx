7e3d991d924b768ee6d8d5d06c23cde3458f7fcc
mroja <mrojaslopez03@gmail.com>
2026-04-08 13:06:11 -0300

    feat: update profile image handling in ActivateAccountPage and localize text in LoginPage and AppLayout

--- src/shared/pages/auth/LoginPage.tsx
+++ src/shared/pages/auth/LoginPage.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "@/modules/auth/components/Footer";
 import LoginForm from "@/modules/auth/components/LoginForm";
-import CustomerDashboardPage from "@/modules/commerce/CommerceDashboardPage";
-import SearchResultsPage from "@/modules/commerce/SearchResultsPage";
+import SearchResultsPage from "@/modules/commerce/components/SearchResultsPage";
 
 export function LoginPage() {
   return (
@@ -12,13 +11,12 @@ export function LoginPage() {
         resetPasswordMessage={"¿Olvidaste tu contraseña?"}
         resetPasswordHref={"/api/users/reset-password/email"}
         registerMessage={"¿No tienes una cuenta?"}
-        linkHrefCommerce={"/register-commerce"}
+        linkHrefCommerce={"/commerce/register-commerce"}
         linkTextCommerce={"Registrate como comercio"}
         linkHrefClient={"/auth/register-client"}
         linkTextClient={"Registrate como cliente"}
       />
 
-      <CustomerDashboardPage />
       <SearchResultsPage />
     </>
   );
