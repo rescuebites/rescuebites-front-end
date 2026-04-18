5013e2a1c8d38ee7ab3bf2c0abf07ce3e1378b91
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2026-04-18 01:38:21 -0300

    ADD EventSource

--- src/modules/navbar/components/CommerceNavbar.tsx
+++ src/modules/navbar/components/CommerceNavbar.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import NavbarUI from "./NavbarUI";
 import { navbarRoutes } from "../config/routes";
 
 export default function CommerceNavbar() {
-    //ver si implementar contador para nro de notif sin leer:
-    // const { notificationCount } = useNotifications();
-  
+  const [notificationCount, setNotificationCount] = useState(0);
+
+  useEffect(() => {
+    console.log("ELFECTO")
+    const eventSource = new EventSource(
+      "http://localhost:8080/subscribe/commerce",
+    );
+    console.log(eventSource);
+    eventSource.addEventListener("notification", (event) => {
+      const data = JSON.parse(event.data);
+      console.log("🔔 Notificación recibida:", data);
+
+      // Incrementar badge
+      setNotificationCount((prev) => prev + 1);
+    });
+
+    eventSource.onerror = (error) => {
+      console.error("❌ Error en SSE:", error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <NavbarUI 
+    <NavbarUI
       routes={navbarRoutes.commerce}
-      notificationCount={0}
+      notificationCount={notificationCount}
     />
   );
 }
